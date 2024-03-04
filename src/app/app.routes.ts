74b9ba9b58cb218c5c7464ad59c0417cd89827dc
Ryan W <28796928+rjwong94@users.noreply.github.com>
2024-03-04 14:06:46 -0700

    RynsRecipes version 1.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'ingredient-list', component: IngredientListComponent},
+  {path: 'recipe-list', component: RecipeComponent},
+  {path: 'recipe-details', component: RecipeDetailsComponent},
+  {path: '', component: RecipeComponent}
+];
