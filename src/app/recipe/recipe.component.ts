571b6491ec6311a2ed1caa32a66e85e29d4a4083
Ryan W <28796928+rjwong94@users.noreply.github.com>
2024-03-05 09:50:31 -0700

    Beginning a top nav bar in preparation for user submissions. Created top-nav component which outlets to the recipe component html (which is where the router outlet defaults to in app.routes)

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RECIPES } from '../stored-recipes';
 import { Recipe } from '../recipes';
 import { RecipeDetailsComponent } from '../recipe-details/recipe-details.component';
+import { TopNavComponent } from '../top-nav/top-nav.component';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [CommonModule, RecipeDetailsComponent],
+  imports: [CommonModule, RecipeDetailsComponent, TopNavComponent],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
