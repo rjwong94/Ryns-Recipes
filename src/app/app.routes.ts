297f8b329dfdacdefdd8442f7037babc250a258d
Ryan W <28796928+rjwong94@users.noreply.github.com>
2024-03-06 16:08:15 -0700

    I have removed the top-nav component entirely and instead settled for having navigation based off the app component html as the home and a router link to the recipe-details component. Down the line, I will need to revise this to be useable with lazy loading and other standard routing practices I am neglecting here. Also, currently the recipe details component is the last child of the view which shows both the ingredients list for the recipe and the steps. Later on, Id like to revise this so that there is also a recipe ingredients list component and a recipe steps component for further separation and scalability. For now, with a quick fix to routing out of the way, I will begin working on a user submitted ingredient component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,9 @@ import { Routes } from '@angular/router';
 import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
 
 export const routes: Routes = [
   {path: 'ingredient-list', component: IngredientListComponent},
   {path: 'recipe-list', component: RecipeComponent},
-  {path: 'recipe-details', component: RecipeDetailsComponent},
-  {path: 'top-nav', component: TopNavComponent},
-  {path: '', component: RecipeComponent}
+  {path: 'recipe-details', component: RecipeDetailsComponent}
 ];
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RECIPES } from '../stored-recipes';
 import { Recipe } from '../recipes';
 import { RecipeDetailsComponent } from '../recipe-details/recipe-details.component';
-import { TopNavComponent } from '../top-nav/top-nav.component';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [CommonModule, RecipeDetailsComponent, TopNavComponent],
+  imports: [CommonModule, RecipeDetailsComponent],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
