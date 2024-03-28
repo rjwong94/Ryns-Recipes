5c3a0ffcdbfbd22d27347b3df7aee42dcecf3499
Ryan W <28796928+rjwong94@users.noreply.github.com>
2024-03-28 17:21:45 -0600

    Routing issue turned out to not be a routing issue. Somehow, me adding the use of the ingredients service in the ingredient list component messed up the entire component. I have commented out the use of the service. I have also added a page not found component and routed a wild card path to the component for later troubleshooting.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { IngredientListComponent } from './modules/ingredient-list/ingredient-list.component';
 import { RecipeComponent } from './modules/recipe/recipe.component';
 import { SubmitIngredientComponent } from './modules/submit-ingredient/submit-ingredient.component';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  {path: 'ingredient-list', component: IngredientListComponent},
-  {path: 'recipe-list', component: RecipeComponent},
-  {path: 'submit-ingredients', component: SubmitIngredientComponent}
+  { path: 'ingredient-list', component: IngredientListComponent },
+  { path: 'recipe-list', component: RecipeComponent },
+  { path: 'submit-ingredients', component: SubmitIngredientComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
