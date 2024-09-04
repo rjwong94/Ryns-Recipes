e6c6539ceceb0515ac4e77536de419b40d27d43e
Ryan W <28796928+rjwong94@users.noreply.github.com>
2024-09-04 16:24:10 -0600

    I have removed the recipe details component and cleaned up the recipes component to reflect the removal. I also added the ingredient amounts and units to the ingredients list of the recipe component, so now the recipes more or less behave the same way they used to with the addition of the accordion ng style. There is a minor issue where the accordion should only be able to keep one recipe open at a time. However, the closeOthers tag in the ngbAccordion doesnt seem to be working as intended.

--- src/app/modules/recipe/recipe.component.ts
+++ src/app/modules/recipe/recipe.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../../core/services/recipes/recipes.interface';
-import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RecipesService } from '../../core/services/recipes/recipes.service';
 import { IngredientsService } from '../../core/services/ingredients/ingredients.service';
 import { Ingredient } from '../../core/services/ingredients/ingredients.interface';
@@ -11,19 +10,14 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [CommonModule, RecipeDetailsComponent, NgbAccordionModule],
+  imports: [CommonModule, NgbAccordionModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
 export class RecipeComponent {
   public recipes: Recipe[] = this._rs.recipes;
-  public selectedRecipeId!: number;
 
   constructor(private _rs: RecipesService, private _is: IngredientsService) {}
-  
-  onSelect(recipeId: number): void {
-    this.selectedRecipeId = recipeId; 
-  }
 
   getIngredient(id: number): Observable<Ingredient | undefined> {
     return this._is.getIngredient(id);
